8fdb74875bf849cfc244ff17ac74308ee40c82ca
hupendrad4 <66543596+hupendrad4@users.noreply.github.com>
2023-04-24 18:27:44 +0530

    Update mySharedLibraryFunction.groovy
    updating sharedRep

--- vars/mySharedLibraryFunction.groovy
+++ vars/mySharedLibraryFunction.groovy
@@ -1,10 +1,5 @@
-def call(Map params) {
-    def currentJob = params.currentJob
-    def currentUser = params.currentUser
-
-    def var_email = "example@jenkins.com" // Replace with your desired value
-
-    println "The value of var_email for ${currentUser} in ${currentJob} is: ${var_email}"
-
-    return var_email
+def call() {
+println("var_email: ${var_email}")
+println("Current login user: ${env.USER}")
+println("Current job name: ${env.JOB_NAME}")
 }
